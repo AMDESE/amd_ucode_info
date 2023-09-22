75bcd097b493ccf5269e9cb91bf5975772d336f4
Eugene Syromiatnikov <esyr@redhat.com>
2023-09-22 16:07:58 +0200

    amd_ucode_info: add --verbose option
    There are (at least) two different situations that require various
    levels of output verbosity:
     * getting some additional auxiliary information beside what
       is provided in the default output (like the patch date), that
       is useful for general examination of the container files;
     * getting all the information available to assist in triaging
       and debugging issues.
    
    Implement --verbose option that increases the verbosity of the output,
    with repeated usage of which increasing the amount of information
    provided.
    
    Note: the date from the data_code field is printed in the ISO 8601
    format to avoid any possible ambiguities in its interpretation.
    
    * amd_ucode_info.py (VERBOSE_DEBUG): New named constant.
    (read_int8): New function.
    (parse_equiv_table): Add opts argument;  read errata_mask,
    errata_compare, and res fields of equivalence table entries instead
    of skipping them;  print them if opts.verbose is set.
    (parse_ucode_file): Pass opts in the parse_equiv_table call;  read the
    data_code, mc_patch_data_id, mc_patch_data_len, init_flag,
    mc_patch_data_checksum, nb_dev_id, sb_dev_id, nb_rev_id, sb_rev_id,
    bios_api_rev, reserved1, and match_reg fields instead of skipping them;
    generate the add_info string based on the verbosity level, print it
    at the end of patch information string;  print the rest of the fields
    if opts.verbose is greater or equal than VERBOSE_DEBUG.
    (parse_options): Add the verbose option.
    * README.md: Update the "./amd_ucode_info.py --help" output.
    
    Signed-off-by: Eugene Syromiatnikov <esyr@redhat.com>

--- amd_ucode_info.py
+++ amd_ucode_info.py
@@ -16,6 +16,9 @@ EQ_TABLE_ENTRY_SIZE = 16
 EQ_TABLE_LEN_OFFSET = 8
 EQ_TABLE_OFFSET = 12
 
+VERBOSE_DEBUG = 2
+
+
 def read_int32(ucode_file):
     """ Read four bytes of binary data and return as a 32 bit int """
     return int.from_bytes(ucode_file.read(4), 'little')
@@ -24,7 +27,11 @@ def read_int16(ucode_file):
     """ Read two bytes of binary data and return as a 16 bit int """
     return int.from_bytes(ucode_file.read(2), 'little')
 
-def parse_equiv_table(ucode_file, eq_table_len):
+def read_int8(ucode_file):
+    """ Read one byte of binary data and return as a 8 bit int """
+    return int.from_bytes(ucode_file.read(1), 'little')
+
+def parse_equiv_table(opts, ucode_file, eq_table_len):
     """
     Read equivalence table and return a list of the equivalence ids contained
     """
@@ -36,15 +43,29 @@ def parse_equiv_table(ucode_file, eq_table_len):
     while table_item < table_stop:
         ucode_file.seek(table_item, 0)
 
+        """
+        struct equiv_cpu_entry {
+            u32 installed_cpu;
+            u32 fixed_errata_mask;
+            u32 fixed_errata_compare;
+            u16 equiv_cpu;
+            u16 res;
+        } __packed;
+        """
         cpu_id = read_int32(ucode_file)
-
-        # Skip errata mask and compare fields
-        ucode_file.seek(8, 1)
-
+        errata_mask = read_int32(ucode_file)
+        errata_compare = read_int32(ucode_file)
         equiv_id = read_int16(ucode_file)
+        res = read_int16(ucode_file)
 
         if equiv_id != 0:
             table[equiv_id] = cpu_id
+
+        if opts.verbose >= VERBOSE_DEBUG:
+            print((" [equiv entry@%#010x: cpuid %#010x, equiv id %#06x, " +
+                   "errata mask %#010x, errata compare %#010x, res %#06x]") %
+                  (table_item, cpu_id, equiv_id, errata_mask, errata_compare,
+                   res))
 
         table_item += EQ_TABLE_ENTRY_SIZE
 
@@ -95,7 +116,7 @@ def parse_ucode_file(opts, path):
         ucode_file.seek(EQ_TABLE_LEN_OFFSET, 0)
         eq_table_len = read_int32(ucode_file)
 
-        ids = parse_equiv_table(ucode_file, eq_table_len)
+        ids = parse_equiv_table(opts, ucode_file, eq_table_len)
 
         cursor = EQ_TABLE_OFFSET + eq_table_len
         while cursor < end_of_file:
@@ -110,10 +131,46 @@ def parse_ucode_file(opts, path):
                 break
 
             patch_length = read_int32(ucode_file)
-            ucode_file.seek(4, 1)
+
+            """
+            struct microcode_header_amd {
+                u32 data_code;
+                u32 patch_id;
+                u16 mc_patch_data_id;
+                u8  mc_patch_data_len;
+                u8  init_flag;
+                u32 mc_patch_data_checksum;
+                u32 nb_dev_id;
+                u32 sb_dev_id;
+                u16 processor_rev_id;
+                u8  nb_rev_id;
+                u8  sb_rev_id;
+                u8  bios_api_rev;
+                u8  reserved1[3];
+                u32 match_reg[8];
+            } __packed;
+            """
+            data_code = read_int32(ucode_file)
             ucode_level = read_int32(ucode_file)
-            ucode_file.seek(16, 1)
+            mc_patch_data_id = read_int16(ucode_file)
+            mc_patch_data_len = read_int8(ucode_file)
+            init_flag = read_int8(ucode_file)
+            mc_patch_data_checksum = read_int32(ucode_file)
+            nb_dev_id = read_int32(ucode_file)
+            sb_dev_id = read_int32(ucode_file)
             equiv_id = read_int16(ucode_file)
+            nb_rev_id = read_int8(ucode_file)
+            sb_rev_id = read_int8(ucode_file)
+            bios_api_rev = read_int8(ucode_file)
+            reserved1 = [read_int8(ucode_file) for _ in range(3)]
+            match_reg = [read_int32(ucode_file) for _ in range(8)]
+
+            if opts.verbose:
+                add_info = " Start=%u bytes Date=%04x-%02x-%02x Equiv_id=%#06x" % \
+                           (patch_start, data_code & 0xffff, data_code >> 24,
+                            (data_code >> 16) & 0xff, equiv_id)
+            else:
+                add_info = ""
 
             if equiv_id not in ids:
                 print("Patch equivalence id not present in equivalence table (%#06x)"
@@ -133,8 +190,22 @@ def parse_ucode_file(opts, path):
 
             stepping = cpu_id & 0xf
 
-            print("  Family=%#04x Model=%#04x Stepping=%#04x: Patch=%#010x Length=%u bytes"
-                  % (family, model, stepping, ucode_level, patch_length))
+            print("  Family=%#04x Model=%#04x Stepping=%#04x: Patch=%#010x Length=%u bytes%s"
+                  % (family, model, stepping, ucode_level, patch_length, add_info))
+
+            if opts.verbose >= VERBOSE_DEBUG:
+                print(("   [data_code=%#010x, mc_patch_data_id=%#06x, " +
+                       "mc_patch_data_len=%#04x, init_flag=%#04x, " +
+                       "mc_patch_data_checksum=%#010x]") %
+                      (data_code, mc_patch_data_id, mc_patch_data_len,
+                       init_flag, mc_patch_data_checksum))
+                print(("   [nb_dev_id=%#010x, sb_dev_id=%#010x, " +
+                       "nb_rev_id=%#04x, sb_rev_id=%#04x, " +
+                       "bios_api_rev=%#04x, reserved=[%#04x, %#04x, %#04x]]") %
+                      (nb_dev_id, sb_dev_id, nb_rev_id, sb_rev_id,
+                       bios_api_rev, reserved1[0], reserved1[1], reserved1[2]))
+                print("   [match_reg=[%s]]" %
+                      ", ".join(["%#010x" % x for x in match_reg]))
 
             if opts.extract:
                 extract_patch(opts, patch_start, patch_length, ucode_file,
@@ -152,6 +223,10 @@ def parse_options():
     parser.add_argument("container_file", nargs='+')
     parser.add_argument("-e", "--extract",
                         help="Dump each patch in container to the specified directory")
+    parser.add_argument("-v", "--verbose", action="count", default=0,
+                        help="Increase output verbosity level: provide once " +
+                             "to see additional information about patches, " +
+                             "twice to see all the information available")
     opts = parser.parse_args()
 
     for f in opts.container_file:
